090b761fede8064fade33d385cfe765b307af651
roy <royraz240i@gmail.com>
2026-03-15 09:56:40 +0200

    Format demo script

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 import wolt
 graph = wolt.Graph({'A': {'B': 4, 'C': 2}, 
-           'B': {'D': 5, 'A': 4},
+           'B': {'D': 5, 'A': 4, 'C': 1},
            'C': {'B': 1, 'D': 8, 'A': 2}, 
            'D': {'E': 2, 'C': 8, 'B': 5},
            'E': {'D' : 2},
@@ -19,15 +19,10 @@ pathfinder = wolt.PathFinder(graph)
 
 assigned_driver = dispatcher.get_active_requests()[request]
 
-print(f"""The request from {request.pickup_location} to
-      {request.dropoff_location} was assigned to driver
-      {assigned_driver.name}. The route they should take to the pickup
-      location is
-      {pathfinder.shortest_path(assigned_driver.current_location,
-      request.pickup_location)}.
-      Then to get to the dropoff location they should take 
-      {pathfinder.shortest_path(request.pickup_location,
-      request.dropoff_location)}. The total distance will be
-        {pathfinder.shortest_distance(assigned_driver.current_location,
+print(f"The request from {request.pickup_location} to {request.dropoff_location} was assigned to driver {assigned_driver.name}.")
+print(f"The route they should take to the pickup location is {pathfinder.shortest_path(assigned_driver.current_location, request.pickup_location)}.")      
+print(f"Then to get to the dropoff location they should take {pathfinder.shortest_path(request.pickup_location,
+      request.dropoff_location)}.")
+print(f"The total distance will be {pathfinder.shortest_distance(assigned_driver.current_location,
       request.pickup_location) + pathfinder.shortest_distance(request.pickup_location,
-      request.dropoff_location)}.""")
+      request.dropoff_location)}.")
