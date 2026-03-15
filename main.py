ce27e8fffcf2471ec09b9cadca97e3e16e5f1cdf
roy <royraz240i@gmail.com>
2026-03-15 16:40:36 +0200

    Add request POST

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import graph_algos
 import dispatch
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
+from datetime import datetime
 
 class Location(BaseModel):
     name: str
@@ -14,6 +15,11 @@ class Road(BaseModel):
 class Driver(BaseModel):
     name: str
     location: Location
+
+class DeliveryRequest(BaseModel):
+    pickup_location: Location
+    dropoff_location: Location
+
 
 app = FastAPI()
 
@@ -68,3 +74,17 @@ def get_shortest_path(start: str, end: str):
     distance = path_finder.shortest_distance(start, end)
     return {"path": path.split('->'),
             "distance": distance}
+
+@app.post("/requests", status_code=status.HTTP_201_CREATED)
+def add_request(request: DeliveryRequest):
+    global num_of_requests
+    pickup = request.pickup_location.name
+    dropoff = request.dropoff_location.name
+    if not (locations.location_exists(pickup) and locations.location_exists(dropoff)):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="one or more of the location does not exist")
+    dispatcher.requests[dispatch.DeliveryRequest(num_of_requests, pickup,
+                                                  dropoff, datetime.now())] = None
+    num_of_requests += 1 
+    return request
+
