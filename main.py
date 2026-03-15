e2a5dd561e4cead4ae6f9cfdda27b2f152040f2a
roy <royraz240i@gmail.com>
2026-03-15 15:49:27 +0200

    Add add location POST

--- main.py
+++ main.py
@@ -26,6 +26,14 @@ num_of_requests = 0
 def root():
     return {"graph": locations.adjs, "drivers": dispatcher.drivers}
 
+@app.post("/locations/", status_code=status.HTTP_201_CREATED)
+def add_location(location: Location):
+    if locations.location_exists(location.name):
+        raise HTTPException(status_code = status.HTTP_409_CONFLICT,
+                            detail="location already exists")
+    locations.add_location(location.name)
+    return location
+
 
     
     
